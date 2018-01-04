6c44dbb39cc471fe331723216d609f8af0daf8a2
wuliao522 <18021071510@163.com>
2018-01-04 14:04:56 +0800

    create

---
+++ DingTalk/Controllers/ApprovalController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebZhongZhi.Controllers
+{
+    public class ApprovalController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OfficeSupplies()
+        {
+            return View();
+        }
+
+        public ActionResult UseCar()
+        {
+            return View();
+        }
+
+        public ActionResult WorkOvertime
+            ()
+        {
+            return View();
+        }
+    }
+}
---
+++ DingTalk/Controllers/MainController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebZhongZhi.Controllers
+{
+    public class MainController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Approval()
+        {
+            return View();
+        }
+
+        public ActionResult Approval_waitMe()
+        {
+            return View();
+        }
+
+        public ActionResult Approval_IDone()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult Approval_fromMe()
+        {
+            return View();
+        }
+
+        public ActionResult Approval_copyMe()
+        {
+            return View();
+        }
+        public ActionResult Main6()
+        {
+            return View();
+        }
+        public ActionResult Main7()
+        {
+            return View();
+        }
+        public ActionResult Main8()
+        {
+            return View();
+        }
+    }
+}
