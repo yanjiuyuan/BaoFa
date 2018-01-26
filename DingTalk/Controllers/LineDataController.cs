77d18be48bc4bfab57c1af707dcbaa9966d4bf3f
caixingtong <634247979@qq.com>
2018-01-26 15:26:44 +0800

    新增webSocks

---
+++ DingTalk/Controllers/LineDataController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.WebSockets;
+
+namespace DingTalk.Controllers
+{
+    
+    [RoutePrefix("api/dt")]
+    public class LineDataController : ApiController
+    {
+        [Route("GetAll")]
+        public string GetAll()
+        {
+            return "123";
+        }
+
+        [Route("Get")]
+        public HttpResponseMessage Get()
+        {
+            if (HttpContext.Current.IsWebSocketRequest)
+            {
+                HttpContext.Current.AcceptWebSocketRequest(ProcessWSChat);
+            }
+            return new HttpResponseMessage(HttpStatusCode.SwitchingProtocols);
+        }
+
+        [Route("ProcessWSChat")]
+        private async Task ProcessWSChat(AspNetWebSocketContext arg)
+        {
+            WebSocket socket = arg.WebSocket;
+            while (true)
+            {
+                ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+                WebSocketReceiveResult result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+                if (socket.State == WebSocketState.Open)
+                {
+                    string message = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+                    string returnMessage = "You send :" + message + ". at" + DateTime.Now.ToLongTimeString();
+
+
+                    buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(returnMessage));
+                    await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
