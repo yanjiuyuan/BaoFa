065cda663a7b383e760a4e19b903bb2087aab3e0
caixingtong <634247979@qq.com>
2018-01-28 17:14:08 +0800

    无修改。

--- DingTalk/Controllers/LineDataController.cs
+++ DingTalk/Controllers/LineDataController.cs
@@ -15,7 +15,7 @@ using System.Web.WebSockets;
 
 namespace DingTalk.Controllers
 {
-    
+
     [RoutePrefix("api/dt")]
     public class LineDataController : ApiController
     {
@@ -30,6 +30,12 @@ namespace DingTalk.Controllers
             return new HttpResponseMessage(HttpStatusCode.SwitchingProtocols);
         }
 
+
+        /// <summary>
+        /// WebSocket接口
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
         [Route("ProcessWSChat")]
         private async Task ProcessWSChat(AspNetWebSocketContext arg)
         {
@@ -45,7 +51,7 @@ namespace DingTalk.Controllers
 
                     //string returnMessage = GetSpray();
 
-                    
+
                     //string returnSpray = GetSpray();
                     string returnVamp = GetTableByTableName("Vamp");
                     string returnWaio = GetTableByTableName("Waio");
@@ -59,9 +65,10 @@ namespace DingTalk.Controllers
                     dString.Add("WaiT", returnWaiT);
                     dString.Add("WaiS", returnWaiS);
                     dString.Add("Outsole", returnOutsole);
-                    
+
+                    //string[] strList = new string[4] { "Vamp", };
+
                     string returnMessage = JsonConvert.SerializeObject(dString);
-                    //returnMessage = returnMessage.Replace(@"\", "");
                     buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(returnMessage));
                     await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
                 }
@@ -78,7 +85,7 @@ namespace DingTalk.Controllers
         /// </summary>
         /// <returns></returns>
         [Route("GetSpray")]
-        public  string GetSpray()
+        public string GetSpray()
         {
             LineDataServer lineDataServer = new LineDataServer();
             var result = lineDataServer.GetSprayMessage();
@@ -88,13 +95,49 @@ namespace DingTalk.Controllers
 
         [Route("GetTable")]
 
-        public string GetTableByTableName(string strTableName)
+        public static string GetTableByTableName(string strTableName)
         {
             LineDataServer lineDataServer = new LineDataServer();
             var result = lineDataServer.GetTableMessage(strTableName);
             return result;
         }
 
+
+
+        public static string RunAllTask(string[] strTableNames)
+        {
+            int iCount = strTableNames.Length;
+            string strJsonString = string.Empty;
+            Dictionary<string, string> dString = new Dictionary<string, string>();
+
+            try
+            {
+                if (iCount >= 1)
+                {
+                    Task[] tasks = new Task[iCount];
+                    for (int i = 0; i < strTableNames.Length; i++)
+                    {
+                        tasks[i] = Task.Factory.StartNew(() =>
+                        {
+                            dString.Add(strTableNames[i], GetTableByTableName(strTableNames[i]));
+                        });
+                    }
+                    Task.WaitAll(tasks);
+                    strJsonString = JsonConvert.SerializeObject(dString);
+                }
+                else
+                {
+                    strJsonString = "strTableNames数组为空";
+                }
+            }
+            catch (Exception ex)
+            {
+                strJsonString = ex.Message;
+                throw;
+            }
+           
+            return strJsonString;
+        }
 
     }
 }
