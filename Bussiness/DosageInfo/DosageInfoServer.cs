b6797bcac634d612cf9839d5135b8f194dec7915
caixingtong <634247979@qq.com>
2018-02-02 11:05:16 +0800

    新增时时用量统计，用量读取接口、日志组件以及日志类修改、数据库帮助类修改。

---
+++ Bussiness/DosageInfo/DosageInfoServer.cs
@@ -0,0 +1,36 @@
+﻿using Common.DbHelper;
+using Common.JsonHelper;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.DosageInfo
+{
+    public class DosageInfoServer
+    {
+        public string GetDosageInfo(string OrderId, string ChildId)
+        {
+            //string strSearchSql = "SELECT WeiTiaoConsumption,HuChiConsumption,BiaoQianConsumption,DaDiConsumption FROM huabao.`Usage` " +
+            //    " WHERE orderid =@Orderid AND childid =@Childid  ORDER BY CT DESC   LIMIT 0,1";
+            //string strSearchSql = "SELECT WeiTiaoConsumption,HuChiConsumption,BiaoQianConsumption,DaDiConsumption FROM huabao.`Usage` " +
+            //   " WHERE OldN =@OldN  ORDER BY CT DESC   LIMIT 0,1";
+            //MySqlParameter[] parameter = new MySqlParameter[] {
+            //   // new MySqlParameter("@Orderid",MySqlDbType.VarChar),
+            //   // new MySqlParameter ("@Childid",MySqlDbType.VarChar
+            //    new MySqlParameter ("@OldN",MySqlDbType.String),
+            //};
+            string strSearchSql = string.Format("SELECT WeiTiaoConsumption,HuChiConsumption,BiaoQianConsumption,DaDiConsumption FROM huabao.`Usage` WHERE orderid ='{0}' AND childid ='{1}'  ORDER BY CT DESC   LIMIT 0,1", OrderId, ChildId);
+            //string strSearchSql = "SELECT * FROM huabao.`Usage`";
+            DataTable db = Common.DbHelper.MySqlHelper.ExecuteQuery(strSearchSql);
+            string strJsonString = JsonConvert.SerializeObject(db);
+            //string strJsonString = JsonHelper.DataTableToJson(db);
+            return strJsonString;
+        }
+
+    }
+}
---
+++ DingTalk/Controllers/StatisticsController.cs
@@ -0,0 +1,34 @@
+﻿using Bussiness.DosageInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DingTalk.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        /// <summary>
+        /// 时时用量统计
+        /// </summary>
+        /// <param name="OrderId">总订单编号</param>
+        /// <param name="ChildId">子订单编号</param>
+        /// <returns>返回围条用量 护齿用量 标签用量 大底用量</returns>
+        /// 测试数据：/Statistics/GetDosageInfo?OrderId=18-35-12-1&ChildId=362-0102-1
+        public string GetDosageInfo(string OrderId, string ChildId)
+        {
+            DosageInfoServer dServer = new DosageInfoServer();
+            return dServer.GetDosageInfo(OrderId, ChildId);
+        }
+
+    }
+}
