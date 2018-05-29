8823d3a2cc6e049c9043a746dbbcaac7df8c6f71
hhhuuuhu <hhhuuuhu@126.com>
2018-05-29 08:45:12 +0800

    Revert "增加实时生产节拍查询"
    This reverts commit 8a70fb9a3d065cace3167092c40167d118f7bc00.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Bussiness.ChartBeatServer ChartBeatServer1 = new Bussiness.ChartBeatServer();
+               string  s= ChartBeatServer1.ChartBeatQuery();
+               Console.WriteLine(s);
+               Console.ReadKey();
+ 
+        }
+    }
+}
