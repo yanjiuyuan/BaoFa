d8e0b3783a5f9dacc39ffa5d9e96209fa1c05a7d
hhhuuuhu <hhhuuuhu@126.com>
2018-06-11 14:49:25 +0800

    生产节拍增加 当前状态运行时间

--- Bussiness/Chart/ChartBeatServer.cs
+++ Bussiness/Chart/ChartBeatServer.cs
@@ -33,16 +33,16 @@ namespace Bussiness.Chart
             long begintime = TimeHelper.ConvertDateTimeToInt(DateTime.Now.AddMinutes(qmins));
              string datestr = DateTime.Now.ToLongDateString();
             long todaybegintime = TimeHelper.ConvertDateTimeToInt(Convert.ToDateTime(datestr));
-                if (begintime < todaybegintime)
-                    begintime = todaybegintime;
+                //if (begintime < todaybegintime)
+                //    begintime = todaybegintime;
                 //获取生产线的最新id_usage
-                string strSql1 = " select  max(id_usage) from `usage` where ProductLineId = " + lineid + " limit 1";
+            string strSql1 = " select  max(id_usage) from `usage` where ProductLineId = " + lineid + " limit 1";
             int usage_id = 0;
             object obj= MySqlHelper.GetSingle(strSql1);
             if (obj != null)
                 usage_id = Convert.ToInt32(obj);
            //string dt = TimeHelper.GetDateTimeFrom1970Ticks(1527044472080/1000).ToString();
-            string strSql = "   select a1.*, if (stationstate is null ,'停止',stationstate) as stationstate  from ( select  IF (t2.Jobs is null,'机器','人工') as JobType,stationNAME, round(run_t /if (run_c > 0,run_c,1))as run,round(free_t /if (free_c > 0,free_c,1)) as free," +
+            string strSql = "   select a1.*, if (stationstate is null ,'停止',stationstate) as stationstate , if (currtime is null ,0,currtime) as currtime  from ( select  t2.JobType,t2.LocationSeq,t1.stationNAME, round(run_t /if (run_c > 0,run_c,1))as run,round(free_t /if (free_c > 0,free_c,1)) as free," +
             "round(warn_t /if (warn_c > 0,warn_c,1))as warn from(" +
            " select   sum( if (stationstate = '运行', endtime - startTime, 0))/1000 AS run_t," +
            "sum( if (stationstate = '运行',1, 0))  AS run_c," +
@@ -51,15 +51,16 @@ namespace Bussiness.Chart
              " sum( if (stationstate = '报警', endtime - startTime, 0))/1000 AS warn_t," +
               "  sum( if (stationstate = '报警',1, 0))  AS warn_c , stationNAME" +
              " from huabao.LocationState where   starttime > " + begintime + " and id_usage = " + usage_id + " group by stationNAME ) as t1 left join" +
-             "(select distinct Jobs from  huabao.ArtificialConfig ) as t2 on t1.stationNAME=t2.Jobs ) a1" +
-             " left join ( select  stationNAME,stationstate     from huabao.LocationStatecache where "+
+             "   huabao.locationcfg t2  on t1.stationNAME=t2.StationName ) a1" +
+             " left join ( select  stationNAME,stationstate , (TIMESTAMPDIFF(SECOND, '1970-1-1', NOW())- round(starttime/1000)) as currtime    from huabao.LocationStatecache where " +
              "   id_usage = " + usage_id + "  )b1" +
-             " on a1.stationNAME  =b1.stationNAME order by a1.JobType, a1.stationNAME";
+             " on a1.stationNAME  =b1.stationNAME order by a1.JobType, a1.LocationSeq";
           
           
           
                 DataTable newTb = MySqlHelper.ExecuteQuery(strSql);
-               
+                newTb.PrimaryKey = new DataColumn[] { newTb.Columns[1] };
+
                 strJsonString = JsonHelper.DataTableToJson(newTb);
              
             }
