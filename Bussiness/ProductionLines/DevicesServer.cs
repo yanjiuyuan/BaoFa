6879ec0fe87daa50ef977fef0f76080b95ddcbf3
hhhuuuhu <hhhuuuhu@126.com>
2018-06-21 14:51:43 +0800

    设备管理

---
+++ Bussiness/ProductionLines/DevicesServer.cs
@@ -0,0 +1,181 @@
+﻿using Common.DbHelper;
+using Common.JsonHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.LogHelper;
+using Newtonsoft.Json;
+using System.Collections;
+
+namespace Bussiness.ProductionLines
+{
+    public class DevicesServer
+    {
+
+        private static Logger logger = Logger.CreateLogger(typeof(ProductionLinesServer));
+        public string GetDeviceList(int? ProductLineId, int? CompanyId,   int? GroupId, int? FoundryId,
+            int? devicestat, string KeyWord  , int? PageIndex = 0, int? PageSize = 10)
+        {
+
+            try
+            {
+                int startRow = PageIndex.Value * PageSize.Value;
+                StringBuilder sb = new StringBuilder();
+                sb.Append(" select t.*,t1.ProductLineName,t1.foundryname,t1.companyName,t1.groupName from deviceinfo t" +
+                    " left join locationcfg t2 on t.LocationId=t2.LocationId and t.ProductLineId=t2.ProductLineId " +
+                    " left join" +
+                     "(select a.ProductLineId, a.ProductLineName, b.foundryid, b.foundryname , c.companyid , c.companyName , d.groupid  , d.groupName from productlineinfo a left join foundryinfo b  on a.foundryid = b.foundryid " +
+                 " left join companyinfo c   on b.companyid = c.companyid left join groupinfo d on c.groupid = d.groupid) t1 on t.ProductLineId=t1.ProductLineId");
+               
+               sb.Append(" where 1=1 ");
+                if (ProductLineId != null)
+                {
+                    sb.Append(string.Format(" and t1.ProductLineId='{0}'", ProductLineId));
+                }
+
+                else if (FoundryId != null)
+                {
+                    sb.Append(string.Format(" and t1.FoundryId='{0}'", FoundryId));
+                }
+                else if (CompanyId != null)
+                {
+                    sb.Append(string.Format(" and t1.CompanyId='{0}'", CompanyId));
+                }
+                else if (GroupId != null)
+                {
+                    sb.Append(string.Format(" and t1.GroupId='{0}'", GroupId));
+                }
+                if(devicestat!=null)
+                    sb.Append(string.Format(" and t.DeviceStat={0}", devicestat));
+                if (KeyWord != null)
+                {
+                    string strWhereKeyWord = string.Format(
+                        " and ( DeviceId like  '%{0}%' " +
+                        "   or  DeviceName like  '%{0}%' " +
+                          "   or  DeviceModel like  '%{0}%' " +
+                          "   or  CompanyName like  '%{0}%' " +
+                             "   or  ProductLineName like  '%{0}%' ) ", KeyWord);
+                    sb.Append(strWhereKeyWord);
+                }
+                 
+                sb.Append("order by t1.groupid, t1.companyid, t1.foundryid, t1.ProductLineId, t.LocationId");
+                int iRows = MySqlHelper.ExecuteQuery(sb.ToString()).Rows.Count;
+                string strWhereLimit = string.Format(" LIMIT {0},{1}", startRow, PageSize.Value);
+
+                sb.Append(strWhereLimit);
+                DataTable db = MySqlHelper.ExecuteQuery(sb.ToString());
+                //Dictionary<string, DataTable> dic = new Dictionary<string, DataTable>();
+                //dic.Add("Spray", db);
+                string strJsonString = JsonHelper.DataTableToJson(db, iRows);
+                return strJsonString;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+                return Global.RETURN_ERROR(ex.Message);
+            }
+        }
+
+        public string DeviceAdd(string DeviceId, string DeviceName, string DeviceType, string DeviceModel, string ComAddress
+           , int ProductLineId, int LocationId, string OnlineDate, string OfflineDate, int DeviceStat,string oper)
+
+        {
+            string retstr = string.Empty;
+
+            try
+            {
+                string strTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                //进行MD5 加密
+
+                string strSql = string.Format("insert into  deviceinfo set " +
+                    "DeviceId='{0}',DeviceName='{1}',DeviceType='{2}',DeviceModel='{3}'," +
+                    "ComAddress='{4}',ProductLineId={5},LocationId={6},OnlineDate='{7}'," +
+                    "OfflineDate='{8}',DeviceStat={9},CT='{10}',Operator='{11}' ",
+                      DeviceId,   DeviceName,   DeviceType,   DeviceModel,   ComAddress
+                ,   ProductLineId,   LocationId,   OnlineDate,   OfflineDate,   DeviceStat, strTime, oper);
+
+
+                int anum = MySqlHelper.ExecuteSql(strSql);
+
+                if (anum <= 1)
+                    retstr = Global.RETURN_ERROR("操作失败!");
+                retstr = Global.RETURN_SUCESS;
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+                retstr = Global.RETURN_ERROR(ex.Message);
+            }
+            return retstr;
+
+        }
+
+
+        public string DeviceSave(string DeviceId, string DeviceName, string DeviceType, string DeviceModel, string ComAddress
+           , int ProductLineId, int LocationId, string OnlineDate, string OfflineDate, int DeviceStat, string oper)
+
+        {
+            string retstr = string.Empty;
+
+            try
+            {
+                string strTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                //进行MD5 加密
+
+                string strSql = string.Format("update  deviceinfo set " +
+                    " DeviceName='{1}',DeviceType='{2}',DeviceModel='{3}'," +
+                    "ComAddress='{4}',ProductLineId={5},LocationId={6},OnlineDate='{7}'," +
+                    "OfflineDate='{8}',DeviceStat={9},CT='{10}',Operator='{11}' where DeviceId='{0}'",
+                      DeviceId, DeviceName, DeviceType, DeviceModel, ComAddress
+                , ProductLineId, LocationId, OnlineDate, OfflineDate, DeviceStat, strTime, oper);
+
+
+                int anum = MySqlHelper.ExecuteSql(strSql);
+
+                if (anum <= 1)
+                    retstr = Global.RETURN_ERROR("操作失败!");
+                retstr = Global.RETURN_SUCESS;
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+                retstr = Global.RETURN_ERROR(ex.Message);
+            }
+            return retstr;
+
+        }
+        public string DeviceDel(string DeviceId )
+
+        {
+            string retstr = string.Empty;
+
+            try
+            {
+              
+                string strSql = string.Format("delete from   deviceinfo   where DeviceId='{0}'",
+                      DeviceId );
+
+
+                int anum = MySqlHelper.ExecuteSql(strSql);
+
+                if (anum <= 1)
+                    retstr = Global.RETURN_ERROR("操作失败!");
+                retstr = Global.RETURN_SUCESS;
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+                retstr = Global.RETURN_ERROR(ex.Message);
+            }
+            return retstr;
+
+        }
+        
+    }
+}
--- DingTalk/Controllers/RegisterController.cs
+++ DingTalk/Controllers/RegisterController.cs
@@ -19,34 +19,23 @@ namespace WebZhongZhi.Controllers
 
 
         /// <summary>
-        /// 注册接口（必填字段：验证码，用户名，密码，所属公司Id，所属公司名称）
+        /// 注册接口（必填字段： 用户名 ，所属公司Id，所属公司名称）
         /// </summary>
-        /// <param name="strCode">验证码(必填)</param>
+
         /// <param name="username">用户名(必填)</param>
         /// <param name="strRealName">真实姓名</param>
         /// <param name="strphoneNumber"></param>
-        /// <param name="strPassword">密码(必填)</param>
-        /// <param name="strRegisterTime">注册时间</param>
-        /// <param name="strLastLoginTime">最后登入时间</param>
-        /// <param name="iStatus">状态</param>
         /// <param name="strAddress">地址</param>
         /// <param name="iRole">用户角色
-        //用户角色影响用户权限，用户权限大概有以下几种
-        //默认权限 1 浏览平台基础信息，平台要展示给普通用户的一般信息。如产品介绍信息（普通用户）
-        //2 浏览所有设备的运行状态，查看报表。（厂管理）
-        //3 查看与操作所管理设备（设备管理）-》需要添加字段映射用户和设备
-        //4 查看与操作所有设备（管理员）
         /// <param name="strProvince">省</param>
         /// <param name="strCity">市</param>
         /// <param name="strTelephone">电话</param>
         /// <param name="strOtherContact">其他概要</param>
         /// <param name="strFax">传真</param>
-        /// <param name="iIsActive">是否活跃</param>
-        /// <param name="strLastLoginIp">最后一次登入Ip</param>
         /// <param name="strEmail">邮箱</param>
-        /// <param name="strCompanyId">公司Id</param>
-        /// <param name="strCompanyName">公司名</param>
-        /// 测试数据：Register/CheckRegister?username=sa&strPassword=123&strCompanyId=1&strCompanyName=华宝有限公司
+        /// <param name="strCompanyId">部门Id（产线id/公司id/集团id）</param>
+        /// <param name="strCompanyName">部门名</param>
+        /// 测试数据：Register/CheckRegister?username=sa&strCompanyId=1&strCompanyName=华宝有限公司
         /// <returns></returns>
         public string CheckRegister(string username, string strRealName,
             string strphoneNumber,  
